482ca8159b564ae67535cdb044221581ff622160
Xatra1 <xatra169@gmail.com>
2024-05-03 21:12:50 -0400

    Fix missing directory issue

--- mkng++.sh
+++ mkng++.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 read -p "Clear the console before compiling? [y]es, [n]o, [c]ancel: " ans
 if [[ "$ans" =~ ^([yY][eE][sS]|[yY])$ ]]; then clear; else if [[ "$ans" =~ ^([cC][aA][nN][cC][eE][lL]|[cC])$ ]]; then exit; fi; fi
+if [ ! -f /src/deb/usr/local/bin/numguesser++ ]; then
+    mkdir src/deb/usr 2>/dev/null
+    mkdir src/deb/usr/local 2>/dev/null
+    mkdir src/deb/usr/local/bin 2>/dev/null
+fi
 make
 make build-pkg
 echo -e '\e[33;1;37mExecuting ng++ with arguments $1 and $2\e[0m'
