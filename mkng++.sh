6cd53cf722ea2df461781558c59b6daf2ecb98f5
Xatra1 <xatra169@gmail.com>
2024-05-18 21:30:25 -0400

    Complete rework of compilation helper

--- mkng++.sh
+++ mkng++.sh
@@ -1,12 +1,56 @@
 #!/bin/bash
-read -p "Clear the console before compiling? [y]es, [n]o, [c]ancel: " ans
 
-if [[ "$ans" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    clear
-else
-    if [[ "$ans" =~ ^([cC][aA][nN][cC][eE][lL]|[cC])$ ]]; then
-        exit
-    fi
+# Behavior variables
+buildpkg=false
+generic=false
+
+# Print the usage document and exit with status code $1
+function usage() {
+    cat <<-EOF
+mkng++ - Helper script for compiling numguesser++
+USAGE: mkng++.sh [-b -g]
+
+-b, --build-pkg         Automatically builds and installs
+                        a Debian package if dpkg is on your
+                        system.
+
+-g, --generic           Installs numguesser++ using the
+                        generic 'install' command. Use this
+                        as a fallback if -b fails.
+
+-h, --help, --usage     Prints this help document and
+                        exits.
+
+If neither -b or -g are specified, both installation methods 
+will be attempted.
+EOF
+    exit $1
+}
+
+# Parse arguments passed to the program
+function argparse() {
+    while [ $# -gt 0 ]; do
+        opt=$1
+        shift
+        case $opt in
+        -b | --build-pkg) buildpkg=true ;;
+        -g | --generic) generic=true ;;
+        -h | --help | --usage) usage 0 ;;
+        *)
+            if [ "${opt:0:1}" = "-" ]; then
+                echo "mkng++: Invalid argument ${opt}"
+                usage 1
+            fi
+            ;;
+        esac
+    done
+}
+
+argparse $*
+
+if [ ! -f ./makefile ]; then
+    echo -e '\e[33;1;31mfatal:\e[0m Makefile not found. Are you in the correct directory?'
+    exit
 fi
 
 if [ ! -f /src/deb/usr/bin/numguesser++ ]; then
@@ -15,14 +59,15 @@ if [ ! -f /src/deb/usr/bin/numguesser++ ]; then
 fi
 
 make
-make build-pkg || make install
-echo -e '\e[33;1;37mExecuting ng++ with arguments $1 and $2\e[0m'
-read -p "Clear the console before executing? [y]es, [n]o: " ans
 
-if [[ "$ans" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    clear
-else
-    echo
+if [ $buildpkg = true ]; then
+    make build-pkg
 fi
 
-ng++ $*
+if [ $generic = true ]; then
+    make install
+fi
+
+if [ $buildpkg = false -a $generic = false ]; then
+    make build-pkg || make install
+fi
